339ab4678612121249638dd5dd9acec023c9a43d
vonadug <36480925+vonadug@users.noreply.github.com>
2026-06-14 17:33:56 +0200

    Add portfolio_config

--- portfolio_config.py
+++ portfolio_config.py
@@ -1,1 +1,43 @@
+# =========================
+# CRYPTO
+# =========================
 
+CRYPTO_HOLDINGS = {
+    "TRX": 9481.90320483,
+    "SOL": 14.53053673,
+    "ADA": 5088.68027346,
+    "ETH": 0.30744651,
+    "BNB": 0.83274646,
+    "DOGE": 505.93792929,
+}
+
+
+# =========================
+# ROBUR
+# =========================
+
+ROBUR_UNITS = 372.7126
+ROBUR_AVG_PRICE_EUR = 23.80
+
+
+# =========================
+# TRADING212
+# =========================
+
+CASH_EUR = 112.19
+
+STOCK_POSITIONS = [
+    ("ETF", "VUSA GBP", "VUSA.L", 29.50884384, 84.0511, "GBP"),
+    ("ETF", "VUSA EUR", "VUSA.AS", 22.34437608, 114.794, "EUR"),
+
+    ("Quantum", "IONQ", "IONQ", 52.13082672, 23.28, "USD"),
+    ("Quantum", "QBTS", "QBTS", 43.13487336, 3.21, "USD"),
+    ("Quantum", "RGTI", "RGTI", 48.268315, 2.98, "USD"),
+
+    ("Growth", "Alphabet", "ABEA.DE", 2.24205794, 168.64, "EUR"),
+    ("Growth", "Apple", "AAPL", 1.82665938, 202.10, "USD"),
+    ("Growth", "Amazon", "AMZ.DE", 1.94746704, 180.38, "EUR"),
+    ("Growth", "Meta", "FB2A.DE", 0.62208621, 533.30, "EUR"),
+    ("Growth", "Microsoft", "MSF.DE", 0.89746218, 399.56, "EUR"),
+    ("Growth", "Nvidia", "NVD.DE", 2.70565948, 113.41, "EUR"),
+]

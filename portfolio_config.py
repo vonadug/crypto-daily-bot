a27d6448fdb2c5e402de8bbd38bc06295de3a132
vonadug <36480925+vonadug@users.noreply.github.com>
2026-06-30 10:30:02 +0200

    Update portfolio_config.py

--- portfolio_config.py
+++ portfolio_config.py
@@ -4,12 +4,12 @@
 # =========================
 
 CRYPTO_HOLDINGS = {
-    "TRX": 9481.90320483,
-    "SOL": 14.53053673,
-    "ADA": 5088.68027346,
-    "ETH": 0.30744651,
-    "BNB": 0.83274646,
-    "DOGE": 505.93792929,
+    "TRX": 9491.90320483,
+    "SOL": 14.56053673,
+    "ADA": 5092.68027346,
+    "ETH": 0.30774651,
+    "BNB": 0.83284646,
+    "DOGE": 505.96792929,
 }
 
 
@@ -19,9 +19,9 @@ CRYPTO_HOLDINGS = {
 # Update NAV manually if Swedbank API is unavailable
 # =========================
 
-ROBUR_UNITS = 372.7126
-ROBUR_NAV = 28.011
-ROBUR_AVG_PRICE_EUR = 23.80
+ROBUR_UNITS = 374.5756
+ROBUR_NAV = 26.750
+ROBUR_AVG_PRICE_EUR = 23.81
 
 
 # =========================
@@ -33,18 +33,18 @@ CASH_EUR = 112.19
 
 STOCK_POSITIONS = [
     ("ETF", "VUSA GBP", "VUSA.L", 29.50884384, 84.0511, "GBP"),
-    ("ETF", "VUSA EUR", "VUSA.AS", 22.34437608, 114.794, "EUR"),
+    ("ETF", "VUSA EUR", "VUSA.AS", 23.1595811, 114.794, "EUR"),
 
-    ("Quantum", "IONQ", "IONQ", 52.13082672, 23.28, "USD"),
-    ("Quantum", "QBTS", "QBTS", 43.13487336, 3.21, "USD"),
-    ("Quantum", "RGTI", "RGTI", 48.268315, 2.98, "USD"),
+    ("Quantum", "IONQ", "IONQ", 52.72023855, 23.28, "USD"),
+    ("Quantum", "QBTS", "QBTS", 43.27044534, 3.21, "USD"),
+    ("Quantum", "RGTI", "RGTI", 48.42251765, 2.98, "USD"),
 
-    ("Growth", "Alphabet", "ABEA.DE", 2.24205794, 168.64, "EUR"),
-    ("Growth", "Apple", "AAPL", 1.82665938, 202.10, "USD"),
-    ("Growth", "Amazon", "AMZ.DE", 1.94746704, 180.38, "EUR"),
-    ("Growth", "Meta", "FB2A.DE", 0.62208621, 533.30, "EUR"),
-    ("Growth", "Microsoft", "MSF.DE", 0.89746218, 399.56, "EUR"),
-    ("Growth", "Nvidia", "NVD.DE", 2.70565948, 113.41, "EUR"),
+    ("Growth", "Alphabet", "ABEA.DE", 2.26684399, 168.64, "EUR"),
+    ("Growth", "Apple", "AAPL", 1.85749719, 202.10, "USD"),
+    ("Growth", "Amazon", "AMZ.DE", 1.98239526, 180.38, "EUR"),
+    ("Growth", "Meta", "FB2A.DE", 0.63673601, 533.30, "EUR"),
+    ("Growth", "Microsoft", "MSF.DE", 0.91873877, 399.56, "EUR"),
+    ("Growth", "Nvidia", "NVD.DE", 2.74596425, 113.41, "EUR"),
     
     ("2026 Finds", "ASML", "ASML.AS", 0.00629119, 1669.00, "EUR"),
     ("2026 Finds", "Franklin Korea", "FLRK.L", 0.10769967, 84.03, "GBP"),
